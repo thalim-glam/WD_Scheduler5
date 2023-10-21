235b9a88c5006d62d78b938cdb7017b94445a778
Tasneem <thalim.glam@gmail.com>
2023-10-20 22:39:11 -0700

    Working on dayjs

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// The jQuery starts from here -
+
 $(function () {
-  //jS code goes here
-  // 
   $(".saveBtn").on("click", function () {
     const hourBlock = $(this).parent().attr("id")
     const note = $(this).siblings(".description").val()
@@ -13,12 +10,7 @@ $(function () {
     $(`#hour-${i} .description`).val(localStorage.getItem(`hour-${i}`))
   }
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // TODO: Add a listener for click events on the save button.
   //
   function timeUpdate() {
     let currentTime = dayjs().hour()
@@ -37,7 +29,10 @@ $(function () {
   }
   timeUpdate()
 
-  $("#currentDay").text(moment().format('[Today: ] LLLL'))
+  // $("#currentDay").text(moment().format('[Today: ] LLLL'))
+  //  dayjs.extend(LocalizedFormat)
+
+  $("#currentDay").text(dayjs().format('[Today: ] dddd, MMMM D, YYYY h:mm A'))
 
   // Button function to clear local storage and clear contents
   $("#clearFieldsBtn").click(function (event) {
@@ -46,11 +41,6 @@ $(function () {
     localStorage.clear();
   });
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
